adfa7a0264b71efb543d70a2091e623fa9ed562f
matlam27 <117271454+matlam27@users.noreply.github.com>
2023-01-17 15:45:14 +0100

    zebank

---
+++ www/index_zebank.php
@@ -0,0 +1,15 @@
+<?php
+session_start();
+$page_title = 'Home page';
+require_once __DIR__ . '/../src/html_head_zebank.php';
+
+?>
+<body>
+
+<div>
+    <h1>ZEBANK</h1>
+</div>
+
+<?php require_once __DIR__ . '/../src/html_footer_zebank.php'; ?>
+</body>
+</html>
