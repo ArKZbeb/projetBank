b26a1df4b6fd04f9411fa58d4564368f63e10c9f
matlam27 <117271454+matlam27@users.noreply.github.com>
2023-01-17 22:26:04 +0100

    modif

--- src/templates/partials/html_head_zebank.php
+++ src/templates/partials/html_head_zebank.php
@@ -5,6 +5,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $page_title; ?></title>
-    <link rel="stylesheet" href="../src/style.css">
     
+    <a href="../www/index_zebank.php"><img src="../logo.png"></a>
+    <nav>
+        <a href="../www/inscription.php">S'INSCRIRE </a>
+        <a href="../www/connexion.php">SE CONNECTER </a>
+    </nav>
 </head>
--- www/index_zebank.php
+++ www/index_zebank.php
@@ -1,20 +1,14 @@
 <?php
 session_start();
 $page_title = 'Home page';
-require_once __DIR__ . '../src/templates/partials/html_head_zebank.php';
+require_once __DIR__ . '/../src/templates/partials/html_head_zebank.php';
 ?>
 <body>
 <link rel="stylesheet" href="../src/style.css">
 <div>
     <h1>💶 ZEBANK 💶</h1>
 </div>
-<div>
-    <a href="../www/inscription.php">S'INSCRIRE </a>
-</div>
-<div>
-    <a href="../www/connexion.php">SE CONNECTER </a>
-</div>
 
-<?php require_once __DIR__ . '../src/templates/partials/html_footer_zebank.php'; ?>
+<?php require_once __DIR__ . '/../src/templates/partials/html_footer_zebank.php'; ?>
 </body>
 </html>
