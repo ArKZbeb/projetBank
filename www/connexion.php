31bc8828fe84b92a167cdff6fabfc73e4cde03d6
Tristan Turgy <116552625+ArKZbeb@users.noreply.github.com>
2023-01-20 12:15:07 +0100

    fix

--- www/connexion.php
+++ www/connexion.php
@@ -5,6 +5,25 @@ require_once __DIR__ . '/../src/init.php';
 
 $page_title = 'Connexion';
 
+
+if(isset($_POST['connexion'])){
+
+    $email = $_POST['email'];
+    $password = hash('sha256', $_POST['password']);
+        if($email != '' && $password != ''){
+            $sth = $db->prepare('SELECT * FROM users WHERE email = ? AND password = ?');
+            $sth->execute([$email, $password]);
+            $donnees = $sth->fetch();
+        }
+        if($donnees != false){
+            $_SESSION['connected'] = true;
+            $_SESSION['id'] = $donnees['id_user'];
+            
+            header('Location:./accueil_compte.php');
+            
+        }
+}
+
 ?>
 
 <body>
@@ -12,10 +31,20 @@ $page_title = 'Connexion';
         <h1>Connexion</h1>
     </div>
     <div>
-</br>
-        <h2><a href="connexion_client.php">Espace Client</a></h2>
-</br>
-        <h2><a href="connexion_admin.php">Espace Administrateur</a></h2>
+        <form action="connexion.php" method="post">
+            
+            <div>
+                <label for="email">identifiant</label>
+                <input type="text" name="email" id="email">
+            </div>
+            <div>
+                <label for="password">mot de passe</label>
+                <input type="password" name="password" id="password">
+            </div>
+            <div>
+                <button type="submit" name="connexion">se connecter</button>
+            </div>
+        </form>
     </div>
     <?php require_once __DIR__ . '/../src/templates/partials/html_footer_zebank.php'; ?>
 </body>
